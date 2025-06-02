99557f19a02a4314ce7eaaa5b5aa4c89007a733e
dennismnyaga <dennisnyaga042@gmail.com>
2025-06-02 19:09:08 +0300

    new code

--- Predictions/settings.py
+++ Predictions/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-q_+klufwbot)xnu_0ov8@)6=kk)^piz-!_kr-!!zybs9i(=-o9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://prophetai.kpipesbuilders.co.ke'
+]
 
 
 # Application definition
